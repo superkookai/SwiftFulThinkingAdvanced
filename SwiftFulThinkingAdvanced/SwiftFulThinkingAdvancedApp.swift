15a3ef9cdd5531389a456732d7480b76da045413
Weerawut Chaiyasomboon <weerawut@live.com>
2023-11-23 12:52:02 +0700

    Initial Commit

---
+++ SwiftFulThinkingAdvanced/SwiftFulThinkingAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFulThinkingAdvancedApp.swift
+//  SwiftFulThinkingAdvanced
+//
+//  Created by Weerawut Chaiyasomboon on 23/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFulThinkingAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
